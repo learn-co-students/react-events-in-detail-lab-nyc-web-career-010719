7a77a8a476ad67c77d03ff5142e2f24a1e2534f5
Your Name <github email address>
2019-03-12 17:51:45 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
 // Code CoordinatesButton Component Here
+import React from "react";
+
+class CoordinatesButton extends React.Component {
+
+  handleClick = (e) => {
+    this.props.onReceiveCoordinates([e.clientX, e.clientY])
+  }
+
+  render() {
+    return (
+      <div>
+      <button onClick={(e) => this.handleClick(e)}> I yam a button. I will give you coordinates </button>
+      </div>
+    )
+  }
+}
+export default CoordinatesButton
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,21 @@
 // Code DelayedButton Component Here
+import React from "react";
+
+class DelayedButton extends React.Component {
+
+  handleOnDelayedClick = (e) => {
+    e.persist()
+    setTimeout(() => {
+      this.props.onDelayedClick(e);
+    }, this.props.delay);
+  };
+
+  render() {
+    return (
+      <div>
+      <button onClick={this.handleOnDelayedClick}> I yam a button and I will delay u</button>
+      </div>
+    )
+  }
+}
+export default DelayedButton
